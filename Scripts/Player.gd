8af68b865457c87ef36bae8d42e5fe3664b7d317
RedCoffeeMug <85520034+RedCoffeeMug@users.noreply.github.com>
2023-07-07 18:22:26 -0400

    font

---
+++ Scripts/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var speed = 200
+var velocity = Vector2()
+
+func _physics_process(delta):
+	handle_input()
+
+func handle_input():
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	velocity.normalized()
+	move_and_slide(velocity * speed)
