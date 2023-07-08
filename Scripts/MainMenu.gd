913d9317c4de81bb9693b5127c94e3bd3e011f05
RedCoffeeMug <85520034+RedCoffeeMug@users.noreply.github.com>
2023-07-08 12:56:27 -0400

    Player Animation + credits

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,9 +1,47 @@
 extends Control
 
+onready var CreditsPanel = get_node("CreditsPanel")
 
 func _ready():
-	pass # Replace with function body.
+	#Generating text
+	var copyright_text = ""
+	
+	copyright_text += "Made by [url=https://github.com/RedCoffeeMug]RedCoffeeMug[/url]\n"
+	copyright_text += "Created using the [url=https://godotengine.org/license/]Godot Game Engine[/url]\n"
+	copyright_text += "Uses [url=https://managore.itch.io/m5x7]m5x7 font[/url]"
+	
+	#license
+	var copyright_info = Engine.get_copyright_info()
+	copyright_text += "\n\n===THIRD-PARTY COMPONETS===\n"
+	for part in copyright_info:
+		copyright_text += part["name"] + " - " + part["parts"][0]["license"] + "\n"
+	
+	#license text
+	copyright_text += "\n===LICENSE TEXTS===\n"
+	var licenses = Engine.get_license_info()
+	for license in licenses:
+		copyright_text += "-----" + license + "-----\n" + licenses[license] + "\n\n"
+	
+	#put text in label
+	$CreditsPanel/credits_label.bbcode_text = copyright_text
+	
+	CreditsPanel.hide()
 
 
 func _on_StartButton_pressed():
 	SceneTransition.change_scene("res://Scenes/MainScene.tscn"	)
+
+
+
+
+func _on_Credits_pressed():
+	CreditsPanel.show()
+
+
+func _on_CreditsCloseButton_pressed():
+	CreditsPanel.hide()
+
+
+
+func _on_credits_label_meta_clicked(meta):
+	OS.shell_open(meta)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,15 @@
 extends KinematicBody2D
 
+#movement vars
 var speed = 200
 var velocity = Vector2()
+#animation
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
+
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	handle_input()
@@ -11,16 +19,20 @@ func handle_input():
 	
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		state_machine.travel("walk")
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		state_machine.travel("walk")
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		state_machine.travel("walk")
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		state_machine.travel("walk")
 	velocity.normalized()
 	move_and_slide(velocity * speed)
+	if move_and_slide(velocity).x == 0 and move_and_slide(velocity).y == 0:
+		state_machine.travel("idle")
 
 
 
-func _ready():
-	$Player/AnimationPlayer.play("Idle")
