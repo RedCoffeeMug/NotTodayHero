fcfd3d97a6e7d8c66badbd5210138ec35959cb7c
RedCoffeeMug <85520034+RedCoffeeMug@users.noreply.github.com>
2023-07-08 15:00:05 -0400

    Enemy sprites and animation

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -6,7 +6,10 @@ var _velocity := Vector2.ZERO
 
 onready var _agent: NavigationAgent2D = $NavigationAgent2D
 onready var _player := get_node(path_to_player)
-onready var _timer: Timer = $Timer
+onready var _timer: Timer = $UpdatePathfinding
+
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
 
 
 func _ready() -> void:
@@ -19,11 +22,19 @@ func _physics_process(delta: float) -> void:
 	
 	var direction := global_position.direction_to( _agent.get_next_location())
 	
-	var desired_velocity := direction * 100.0
+	var desired_velocity := direction * 20.0
 	var steering := (desired_velocity - _velocity) * delta * 4.0
 	_velocity += steering
 	
 	_velocity = move_and_slide(_velocity)
+	
+	#animation
+	if move_and_slide(_velocity).x == 0 and move_and_slide(_velocity).y == 0:
+		state_machine.travel("idle")
+	if move_and_slide(_velocity).x < 0:
+		state_machine.travel("walk_left")
+	if move_and_slide(_velocity).x > 0:
+		state_machine.travel("walk_right")
 
 func _update_pathfinding() -> void:
 	_agent.set_target_location(_player.global_position)
